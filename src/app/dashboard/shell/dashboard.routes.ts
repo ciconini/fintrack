4448a7095aa930ad1c1c506188e9a62ca20586b1
Felipe Ciconini <felipe.ciconini@thortful.com>
2024-06-16 17:12:03 +0100

    Added dashboard routes

--- src/app/dashboard/shell/dashboard.routes.ts
+++ src/app/dashboard/shell/dashboard.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "../dashboard.component";
+import { ExpenseListComponent } from "../../expenses/feature/expense-list/expense-list.component";
 
 export const DashboardRoutes: Routes = [
   {
