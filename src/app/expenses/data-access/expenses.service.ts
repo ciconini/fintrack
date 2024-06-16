032fffc10c0515eab39bfea0cf10a28945522170
Felipe Ciconini <felipe.ciconini@thortful.com>
2024-06-16 17:11:07 +0100

    Setup services to get data from backend

--- src/app/expenses/data-access/expenses.service.ts
+++ src/app/expenses/data-access/expenses.service.ts
@@ -1,38 +1,70 @@
 import { Injectable } from '@angular/core';
 import { Expense, ExpenseType } from '../util/model/expense';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, map, of, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../../environments/environment.local';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpensesService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   public getExpenses(): Observable<Expense[]> {
-    const expensesMock: Expense[] = [
-      {
-        id: '1',
-        name: 'Lidl',
-        type: ExpenseType.GROCERY,
-        value: 11.2,
-        date: new Date()
-      },
-      {
-        id: '2',
-        name: 'Pingo Doce',
-        type: ExpenseType.GROCERY,
-        value: 24.99,
-        date: new Date()
-      },
-      {
-        id: '3',
-        name: 'McDonalds',
-        type: ExpenseType.MEAL,
-        value: 9.99,
-        date: new Date()
-      },
-    ]
-    return of(expensesMock)
+    return this.http.get<Expense[]>(`${environment.url}/expenses`).pipe(
+      map(response => {
+        return response
+      }),
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error)
+      })
+    );
+  }
+
+  public getExpense(id: string): Observable<Expense> {
+    return this.http.get<Expense>(`${environment.url}/expenses/${id}`).pipe(
+      map(response => {
+        return response
+      }),
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error)
+      })
+    )
+  }
+
+  public saveExpense(payload: Expense): Observable<Expense> {
+    return this.http.post<Expense>(`${environment.url}/expenses`, payload).pipe(
+      map(response => {
+        return response
+      }),
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error)
+      })
+    )
+  }
+
+  public updateExpense(id: string, payload: any): Observable<Expense> {
+    return this.http.patch<Expense>(`${environment.url}/expense/${id}`, payload).pipe(
+      map(response => {
+        return response
+      }),
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error)
+      })
+    )
+  }
+
+  public deleteExpense(id: string): Observable<Expense> {
+    return this.http.delete<Expense>(`${environment.url}/expense/${id}`).pipe(
+      map(response => {
+        return response
+      }),
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error)
+      })
+    )
   }
 }
