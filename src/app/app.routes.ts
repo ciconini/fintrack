a195bf5363cf5c4d96dfbffc2d41148765118fdc
Felipe Ciconini <felipe.ciconini@thortful.com>
2024-06-16 17:11:53 +0100

    Added routes to other pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/ui/layout/layout.component';
 import { DashboardRoutes } from './dashboard/shell/dashboard.routes';
+import { ExpensesRoutes } from './expenses/shell/expenses.routes';
 
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
     component: LayoutComponent,
     children: DashboardRoutes
+  },
+  {
+    path: 'expenses',
+    component: LayoutComponent,
+    children: ExpensesRoutes
   }
 ];
